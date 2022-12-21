e2539f25598d1da01aedfa69c2c8a2700ee17a91
jedrzej-gorski <jedrzej.gorski@student.put.poznan.pl>
2022-12-21 20:35:37 +0100

    Finish implementing lab and mansion logic. Expand messages to allow for queueing multiple in a specific order. Implement outcomes

---
+++ src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,44 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import javax.swing.JOptionPane;
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            // go !
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+    
+	// TODO: Research - Does this make sense? I know it's simpler to expose the expert system to the entire JOptionPane interface, but doesn't this violate
+	// the intended separation between the user interface and the knowledge base?
+    public static class UserInterface extends JOptionPane {
+
+		private static final long serialVersionUID = 1L;
+		private static final String blurb = "THE APOCALYPSE";
+
+		public static void displayTextbox(String contents, int type) {
+        	showMessageDialog(null, contents, blurb, type);
+        }
+		
+		public static int displayQuestion(String question, String[] answers) {
+			int answer = showOptionDialog(null, question, blurb, 0, PLAIN_MESSAGE, null, answers, null);
+			return answer;
+		}
+    }
+
+
+}
